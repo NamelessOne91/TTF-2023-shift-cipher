a83b19f4da474902d4645eb4b4b257b4b3fcae8e
paolo invernizzi <paolo.invernizzi@emsibg.com>
2023-02-08 17:33:50 +0100

    Add setup for encryption exercise

---
+++ encryption/encrypt.py
@@ -0,0 +1,19 @@
+
+def encrypt(text: str, key: int) -> str:
+    """Applies a shift cipher (also known as Caesar cipher) to the input text,
+    by rotating it LEFT of key number of positions.
+    Returns the rotated text.
+    """
+    pass
+
+
+def _main():
+    # read your secret encryption key from the .env file
+    # read from encrypt_input.txt
+    # call encrypt on each line with your key
+    # write the encrypted lines to encrypt_output.txt
+    pass
+
+
+if __name__ == "__main__":
+    _main()
