95752a0d5c87792e43368b780ea711546ee78bbd
paolo invernizzi <paolo.invernizzi@emsibg.com>
2023-02-08 17:34:07 +0100

    Add setup for decryption exercise

---
+++ decryption/decrypt.py
@@ -0,0 +1,19 @@
+
+def decrypt(text: str, key: int) -> str:
+    """Applies a shift cipher (also known as Caesar cipher) to the input text,
+    by rotating it RIGHT of key number of positions.
+    Returns the rotated text.
+    """
+    pass
+
+
+def _main():
+    # read your secret encryption key from the .env file
+    # read from decrypt_input.txt ---> encrypted with key 3
+    # call decrypt on each line with your key
+    # write the decrypted lines to decrypt_output.txt
+    pass
+
+
+if __name__ == "__main__":
+    _main()
