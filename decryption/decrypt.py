7e14d3b8d0e6c0ae917e231ecaba99fe8138bfce
ishek01 <atzorigianmarco@gmail.com>
2023-02-09 14:16:40 +0100

    Modifica decrypt

--- decryption/decrypt.py
+++ decryption/decrypt.py
@@ -1,23 +1,23 @@
 from dotenv import load_dotenv
 import os
 
-def decrypt(text: str, ) -> str:
-    """Applies a shift cipher (also known as Caesar cipher) to the input text,
-    by rotating it RIGHT of key number of positions.
-    Returns the rotated text.
-    """
-    pass
-
-
-def _main():
-    # read your secret encryption key from the .env file
-    k = os.getenv("CIPHER_KEY")
-    # read from decrypt_input.txt ---> encrypted with key 3
-    # call decrypt on each line with your key
-    # write the decrypted lines to decrypt_output.txt
-    pass
-
-
-
-if __name__ == "__main__":
-    _main()
+def decrypt(riga, key) -> str:
+    riga = list(riga)
+    testoDecrifrato = ""
+    for c in riga:
+        if c.isalpha():
+            sposta_c = chr((ord(c.upper()) - 65 - key) % 26 + 65)
+            if c.islower():
+                sposta_c = sposta_c.lower()
+            testoDecrifrato += sposta_c
+        else:
+            testoDecrifrato += c
+    return testoDecrifrato
+    
+load_dotenv()
+k = int(os.getenv("CIPHER_KEY"))
+with open("decrypt_input.txt") as input:
+    testoCriptato = input.readlines()
+testoDecifrato = [decrypt(linea, k) for linea in testoCriptato]
+with open("output.txt", "w") as output:
+    output.writelines(testoDecifrato)
