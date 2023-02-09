31c47214f4abff1150993190aa4701f67e839b73
paolo invernizzi <paolo.invernizzi@emsibg.com>
2023-02-09 11:25:15 +0100

    Add dotenv and read/write files examples

--- encryption/encrypt.py
+++ encryption/encrypt.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+import os
+
 
 def encrypt(text: str, key: int) -> str:
     """Applies a shift cipher (also known as Caesar cipher) to the input text,
@@ -8,11 +11,18 @@ def encrypt(text: str, key: int) -> str:
 
 
 def _main():
+    load_dotenv()
     # read your secret encryption key from the .env file
+    k = os.getenv("CIPHER_KEY")
+    print(type(k))
     # read from encrypt_input.txt
+    with open("./encryption/encrypt_input.txt") as input:
+        print(input.readlines())
     # call encrypt on each line with your key
-    # write the encrypted lines to encrypt_output.txt
-    pass
+    # write the encrypted lines to encrypt_output.txtc
+    with open("./encryption/encrypt_output.txt", "w") as output:
+        output.write("TEST")
+   
 
 
 if __name__ == "__main__":
