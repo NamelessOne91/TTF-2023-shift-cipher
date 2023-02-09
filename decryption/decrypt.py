87742f4b72b6b078817879528156db6eb138bf10
paolo invernizzi <paolo.invernizzi@emsibg.com>
2023-02-09 16:53:42 +0100

    Implement encrypt and decrypt functions

--- decryption/decrypt.py
+++ decryption/decrypt.py
@@ -1,10 +1,19 @@
 
+from string import ascii_lowercase
+
+
 def decrypt(text: str, key: int) -> str:
     """Applies a shift cipher (also known as Caesar cipher) to the input text,
     by rotating it RIGHT of key number of positions.
     Returns the rotated text.
     """
-    pass
+    if type(key) is not int or key < 0:
+        raise ValueError("key must be >= 0")
+
+    rotated = ascii_lowercase[-key:] + ascii_lowercase[:-key]
+    table = str.maketrans(ascii_lowercase, rotated)
+
+    return text.translate(table)
 
 
 def _main():
--- encryption/encrypt.py
+++ encryption/encrypt.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
-import os
+from string import ascii_lowercase
+
 
 
 def encrypt(text: str, key: int) -> str:
@@ -7,21 +8,22 @@ def encrypt(text: str, key: int) -> str:
     by rotating it LEFT of key number of positions.
     Returns the rotated text.
     """
-    pass
+    if type(key) is not int or key < 0:
+        raise ValueError("key must be an int >= 0")
+
+    rotated = ascii_lowercase[key:] + ascii_lowercase[:key]
+    table = str.maketrans(ascii_lowercase, rotated)
+
+    return text.translate(table)
+
 
 
 def _main():
-    load_dotenv()
     # read your secret encryption key from the .env file
-    k = os.getenv("CIPHER_KEY")
-    print(type(k))
     # read from encrypt_input.txt
-    with open("./encryption/encrypt_input.txt") as input:
-        print(input.readlines())
     # call encrypt on each line with your key
     # write the encrypted lines to encrypt_output.txtc
-    with open("./encryption/encrypt_output.txt", "w") as output:
-        output.write("TEST")
+   pass
    
 
 
